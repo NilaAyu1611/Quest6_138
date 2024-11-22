646b82f12a465caeff09ac0716d386d7ecd4bc72
NilaAyu1611 <nilaayucw@gmail.com>
2024-11-22 19:20:31 +0700

    Membuat class RencanaStudyViewModel, dengan variabel/properti yang sudah dibuat sebelumnya, dan untuk mendeklarasikan statenya, jika ada perubahan maka otomatis akan diperbarui.

--- app/src/main/java/com/example/latihanhalaman/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/latihanhalaman/view/viewmodel/RencanaStudyViewModel.kt
@@ -9,4 +9,24 @@ import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel(){
 
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status ->
+            status.copy(
+                namaMK = ls[0],
+                kelas = ls[1]
+            )
+        }
+    }
+
 }
