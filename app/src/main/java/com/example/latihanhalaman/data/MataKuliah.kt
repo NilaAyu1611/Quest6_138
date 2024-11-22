d96d4d826c183a670815b84cdc2fe48799039e31
NilaAyu1611 <nilaayucw@gmail.com>
2024-11-22 19:13:11 +0700

    Membuat data untuk object MataKuliah opsinya/lisnya

--- app/src/main/java/com/example/latihanhalaman/data/MataKuliah.kt
+++ app/src/main/java/com/example/latihanhalaman/data/MataKuliah.kt
@@ -1,5 +1,14 @@
 package com.example.latihanhalaman.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+
 
 }
